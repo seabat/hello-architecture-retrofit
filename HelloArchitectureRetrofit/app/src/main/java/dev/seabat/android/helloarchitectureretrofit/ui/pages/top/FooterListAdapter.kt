7ac0c7a5464bf21235c8997563bbb1cead3404b8
seabat <ryouta.nakasnishi.86@gmail.com>
2024-06-23 17:05:57 +0900

    ページネーションのレイアウトを作成する

--- HelloArchitectureRetrofit/app/src/main/java/dev/seabat/android/helloarchitectureretrofit/ui/pages/top/FooterListAdapter.kt
+++ HelloArchitectureRetrofit/app/src/main/java/dev/seabat/android/helloarchitectureretrofit/ui/pages/top/FooterListAdapter.kt
@@ -1,20 +1,56 @@
 package dev.seabat.android.helloarchitectureretrofit.ui.pages.top
 
 import android.view.LayoutInflater
+import android.view.View.GONE
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import dev.seabat.android.helloarchitectureretrofit.databinding.ListitemFooterGithubReopBinding
+import dev.seabat.android.helloarchitectureretrofit.domain.entity.RepositoryListEntity
 
 class FooterListAdapter(
     private val onPageClick: (page: Int) -> Unit
 ) : RecyclerView.Adapter<FooterListAdapter.FooterListViewHolder>() {
 
-    var pageNumber = 0
+    var totalPageNumber = 0
+    var currentPageNumber = 0
+
+    fun updatePageNumber(totalPageNumber: Int, currentPageNumber: Int) {
+        this.totalPageNumber = totalPageNumber
+        this.currentPageNumber = currentPageNumber
+        this.notifyDataSetChanged()
+    }
 
     class FooterListViewHolder(val binding: ListitemFooterGithubReopBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind() {
+        fun bind(totalPageNumber: Int, currentPageNumber: Int, onPageClick: (page: Int) -> Unit) {
+            // ー  1 / 100 ＋
 
+            binding.imageBack.apply {
+                visibility = if (2 <= currentPageNumber) {
+                    VISIBLE
+                } else {
+                    INVISIBLE
+                }
+                setOnClickListener {
+                    onPageClick(currentPageNumber - 1)
+                }
+            }
+
+            binding.imageForward.apply {
+                visibility = if (currentPageNumber < totalPageNumber) {
+                    VISIBLE
+                }  else {
+                    INVISIBLE
+                }
+                setOnClickListener {
+                    onPageClick(currentPageNumber + 1)
+                }
+            }
+
+            binding.textCurrent.text = currentPageNumber.toString()
+            binding.textTotal.text = totalPageNumber.toString()
         }
     }
 
@@ -29,6 +65,6 @@ class FooterListAdapter(
     }
 
     override fun onBindViewHolder(holder: FooterListViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(totalPageNumber, currentPageNumber, onPageClick)
     }
 }
