aaf4b6b5aee51da058ef55a7fca9c60fd53618d5
seabat <ryouta.nakasnishi.86@gmail.com>
2024-07-28 01:52:07 +0900

    1.2 -> 1.3.0

--- HelloArchitectureRetrofit/app/build.gradle.kts
+++ HelloArchitectureRetrofit/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.seabat.android.helloarchitectureretrofit"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
